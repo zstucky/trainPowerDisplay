6fab9d69f63f5ffeebf59c128633e3d849083e81
Zach Stucky <zkstucky@gmail.com>
2025-05-30 14:08:22 -0500

    Sets the guages to serial monitor input values

--- Train_reader.ino
+++ Train_reader.ino
@@ -5,19 +5,35 @@ Servo myservo_amps;
 Servo myservo_volts;
 // twelve Servo objects can be created on most boards
 
-int pos = 170;    // variable to store the servo position
-
 void setup() {
   myservo_watts.attach(9);  // attaches the servo on pin 9 to the Servo object
   myservo_amps.attach(10);
   myservo_volts.attach(11);
   
-  myservo_watts.write(pos);
-  myservo_amps.write(pos);
-  myservo_volts.write(pos);
+  myservo_watts.write(170);
+  myservo_amps.write(170);
+  myservo_volts.write(170);
   
+  Serial.begin(9600);
+  Serial.println("Enter: watts amps volts (e.g. 90 45 135)");
 }
 
 void loop() {
+  if (Serial.available()) {
+    int watts_pos = Serial.parseInt();
+    int amps_pos  = Serial.parseInt();
+    int volts_pos = Serial.parseInt();
 
+    // Wait for full input to be received
+    if (Serial.read() == '\n') {
+      // Clamp to valid servo range
+      watts_pos = constrain(watts_pos, 0, 170);
+      amps_pos  = constrain(amps_pos, 0, 170);
+      volts_pos = constrain(volts_pos, 0, 170);
+
+      myservo_watts.write(watts_pos);
+      myservo_amps.write(amps_pos);
+      myservo_volts.write(volts_pos);
+    }
+  }
 }
