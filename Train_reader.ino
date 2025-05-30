b85592f773400eaf14efc271ec63d6f5b6083736
Zach Stucky <zkstucky@gmail.com>
2025-05-30 13:38:20 -0500

    Sets all 3 servos to correct initial position

--- Train_reader.ino
+++ Train_reader.ino
@@ -1,13 +1,21 @@
 #include <Servo.h>
 
-Servo myservo;  // create Servo object to control a servo
+Servo myservo_watts;  // create Servo object to control a servo
+Servo myservo_amps;
+Servo myservo_volts;
 // twelve Servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
+int pos = 170;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
-  myservo.write(0);
+  myservo_watts.attach(9);  // attaches the servo on pin 9 to the Servo object
+  myservo_amps.attach(10);
+  myservo_volts.attach(11);
+  
+  myservo_watts.write(pos);
+  myservo_amps.write(pos);
+  myservo_volts.write(pos);
+  
 }
 
 void loop() {
